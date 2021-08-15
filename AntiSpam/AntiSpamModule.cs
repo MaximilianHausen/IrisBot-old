e1c4d0b33c467d3498bc8f89a53b0c7f5dd9abda
Max Hausen <Max.Ha@directbox.com>
2021-08-15 12:24:24 +0200

    Add empty module

---
+++ AntiSpam/AntiSpamModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using IrisLoader.Modules;
+
+namespace AntiSpam
+{
+	public class AntiSpamModule : IrisModule
+	{
+		public override bool IsActive(ulong guildId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override Task Load()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void SetActive(ulong guildId, bool state)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override Task Unload()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
